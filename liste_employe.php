e3438e1fa6d77512ba800c62b1264ee45cb7c564
Ghost2107 <110019799+Ghost2107@users.noreply.github.com>
2022-07-26 10:42:17 +0200

    Add files via upload

---
+++ liste_employe.php
@@ -0,0 +1,101 @@
+<?php
+require_once "Connect.php";
+require_once "template.php";
+
+
+$requete_sql_employe = "SELECT * FROM Employe ";
+
+$execution_requete = $db->query($requete_sql_employe);
+
+
+?>
+
+
+
+<div id="page-wrapper" style="min-height: 292px;">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <form class="form-horizontal" data-toggle="validator" role="form" method="get" action="recherche_employe.php">
+                        <input type="text" class=" form-control" name="recherche_employe" id="recherche_employe">
+
+                        <p>
+                            <button type="submit" class="btn btn-primary">Recherche </button>
+                        </p>
+                        Liste des employés
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="dataTable_wrapper">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+
+                                    <th class=" col-sm-1 ">NOM EMPLOYE</th>
+                                    <th class=" col-sm-1">PRENOM</th>
+                                    <th class=" col-sm-1 ">MATRICULE</th>
+                                    <th class=" col-sm-1 ">POSTE</th>
+                                    <th class=" col-sm-1 ">NOM SERVICE</th>
+                                    <th class=" col-sm-1 ">MATRICULE TELEPHONE</th>
+
+
+                                    <th class=" col-sm-3 ">Action</th>
+                                </tr>
+                            </thead>
+                            <?php while ($row = $execution_requete ->fetch(PDO::FETCH_ASSOC)) : ?>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <?php echo $row['nom_employe']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $row['prenom_employe']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $row['matricule_employe']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $row['poste']; ?>
+                                            
+                                        </td>
+                                        <td>
+                                             <?php
+                                            $recup_id_services = $row['id_services'];
+                                            $verif_table_service = "SELECT * FROM `Services` WHERE `id_services`='$recup_id_services'";
+
+
+                                            $stmt = $db->query($verif_table_service);
+                                            $contenu_colonne_service = $stmt->fetch(PDO::FETCH_ASSOC); ?>
+                                        </td>
+                                        <td>   
+                                            
+                                            <?php
+                                            $recup_id_telephone = $row['id_telephone'];
+                                            $verif_table_telephone = "SELECT * FROM `Telephone` WHERE `id_telephone`='$recup_id_telephone'";
+
+
+                                            $stmt = $db->query($verif_table_telephone);
+                                            $contenu_colonne_telephone = $stmt->fetch(PDO::FETCH_ASSOC);
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <a type="button" class="btn btn-warning" href="modifieremp.php?id_employe=<?php echo $row['id_employe']; ?>&id_telephone=<?php echo $row['id_telephone']; ?>&nom_employe=<?php echo $row['nom_employe']; ?>&prenom_employe=<?php echo $row['prenom_employe']; ?>&matricule_employe= <?php echo $row['matricule_employe']; ?>&poste= <?php echo $row['poste']; ?> "><i class="fa fa-edit fa-lg"></i> Editer</a>
+                                            <a type="button" class="btn btn-danger" href="liste_employe.php?id_employe=<?php echo $row['id_employe']; ?>"><i class="fa fa-trash fa-lg"></i> Supprimer</a>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                                </tbody>
+
+                        </table>
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+
+    
---
+++ recharge.php
@@ -0,0 +1,162 @@
+<?php
+require_once "template.php";
+
+require_once "Connect.php";
+
+
+
+
+$sql1 = "SELECT id_carte_sim, numero_sim FROM carte_sim ";
+
+$stmt1 = $db->query($sql1);
+/* 
+var_dump($stmt1);
+exit; */
+
+
+?>
+
+
+
+
+<div id="page-wrapper" style="min-height: 292px;">
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <h3 align="center" class=" bg-success titre-contact"> AJOUTER UNE RECHARGE</h3>
+
+            <div class="panel panel-primary">
+                <form class="form-horizontal" data-toggle="validator" role="form" method="post">
+                    <p></p>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="col-xs-5">
+                                <!--<form class="form-horizontal" role="form">-->
+
+
+
+                                <div class="form-group">
+                                    <label class=" col-sm-4 control-label">CARTE SIM:</label>
+                                    <div class="col-sm-6">
+                                        <select name="id_carte_sim" class="form-control" required>
+
+
+
+                                            /* affiche les id des cartes sim et les numero correspondes *\
+                                            <?php while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)) : ?>
+
+                                                <option value="<?php echo $row['id_carte_sim']; ?>"><?php echo $row['numero_sim']; ?></option>
+
+
+
+
+
+                                            <?php endwhile; ?>
+
+                                        </select>
+
+                                    </div>
+
+
+
+                                </div>
+
+
+
+
+
+
+                            </div><!--  col-xs-5-->
+
+
+                            <div class="col-xs-5">
+
+
+
+
+
+                                <div class="form-group">
+                                    <label class=" col-sm-4 control-label">MONTANT :</label>
+                                    <div class="col-sm-8">
+
+                                        <input type="text" class="form-control" name="montant" id="montant" pattern="[-+]?[0-9]+(\.[0-9]+)?([eE][-+]?[0-9]+)?" title="exemple de farmat 55 ou 55.5 " required>
+
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class=" col-sm-4 control-label">DATE:</label>
+                                    <div class="col-sm-8">
+
+
+                                        <input type="date" class="form-control" name="dates" id="dates" required>
+
+                                    </div>
+                                </div>
+
+
+                            </div><!--  /col-xs-5>-->
+                            <div class="col-xs-2">
+                                <p>
+                                    <button type="submit" class="btn btn-primary" name="enregistrer">Valider </button>
+                                </p>
+
+                            </div>
+                            <!--col-xs-2 -->
+                        </div>
+                        <!--col-xs-12 -->
+
+                    </div>
+                    <!--row -->
+
+                </form> <!--  /form>-->
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php
+    $sql = null;
+    if (isset($_POST['enregistrer'])) {
+
+        $id_carte_sim = $_POST['id_carte_sim'];
+        $montant = $_POST['montant'];
+        $dates = $_POST['dates'];
+
+
+
+        //verifie si le montant contient (-) à la fin ou au debut ou seulement (-)
+        $verif_tiret = (preg_match_all("/^([a-z0-9]+-)*[a-z0-9]+$/i", $montant));
+        //verifie si le montant contient (_) à la fin ou au debut ou seulement (_)
+        $verif_underscore = (preg_match_all("/^([a-z0-9]+_)*[a-z0-9]+$/i", $montant));
+
+
+
+        if (
+            isset($_POST['id_carte_sim']) && isset($_POST['montant'])
+            && isset($_POST['dates'])
+        ) {
+//verifie si le montant est un nombre et un entier positif et superieur à zero
+            $verif_montant_int_positif = (is_int($montant) || ctype_digit($montant)) && (int)$montant > 0 ;
+       
+            
+            if ($verif_montant_int_positif) {
+
+                $sql = " INSERT INTO Recharge (id_carte_sim,montant,created_at)  
+        VALUES ('$id_carte_sim','$montant','$dates') ";
+                // utilise exec() car aucun résultat n'est renvoyé
+                $db->exec($sql);
+                //creation de l'enregistrement
+                echo "<div class='alert alert-success'>";
+                echo  " Nouvel enregistrement crée avec success ";
+                echo " </div>";
+            } else {
+                echo "<div class='alert alert-danger'>";
+                echo  " Nouvel enregistrement refusé   <br>";
+                echo " </div>";
+            }
+        }
+    }
