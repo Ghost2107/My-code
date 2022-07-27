b5ef04fe8bdf887fd2c0091929e302cc4137c06d
Ghost2107 <110019799+Ghost2107@users.noreply.github.com>
2022-07-27 17:30:55 +0200

    Add files via upload

---
+++ liste_service.php
@@ -0,0 +1,103 @@
+<?php
+require_once "Connect.php";
+require_once "template.php";
+$tab = [];
+
+$sql = "SELECT * FROM Services  ";
+
+$stmt = $db->query($sql);
+
+
+
+if (isset($_GET['id_services'])) {
+
+    if (isset($_POST['recherche_service'])) {
+        array_push($tab, 'Champ vide');
+    } else {
+
+
+
+        $id = $_GET['id_services'];
+        $req = "DELETE FROM Services WHERE (id_services ='" . $id . "')";
+        $db->exec($req);
+    }
+}
+
+if ($tab) {
+    var_dump($tab[0]);
+    exit;
+}
+
+
+
+
+?>
+
+
+<style>
+    .recherche_service {
+
+        width: 70%;
+        outline: none;
+        border: 1px solid;
+        height: 30px;
+        background: whitesmoke;
+        border-radius: 7px;
+
+    }
+</style>
+
+<div id="page-wrapper" style="min-height: 292px;">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <form class="form-horizontal" data-toggle="validator" role="form" method="get" action="recherche_service.php">
+                        <input type="text" class=" form-control" name="recherche_service" id="recherche_service">
+
+                        <p>
+                            <button type="submit" class="btn btn-primary">Recherche </button>
+                        </p>
+
+
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="dataTable_wrapper">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+
+                                    <th class=" col-sm-1 ">NOM SERVICE</th>
+                                    <th class=" col-sm-2 ">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+                                    <tr>
+
+                                        <td>
+                                            <?php echo $row['nom_services']; ?>
+                                        </td>
+                                        <td>
+
+                                            <a type="button" class="btn btn-warning" href="modifier.php?id_services=<?php echo $row['id_services']; ?>&nom_services=<?php echo $row['nom_services']; ?>"><i class="fa fa-edit fa-lg"></i> Editer</a>
+                                            <a type="button" class="btn btn-danger" href="liste_service.php?id_services=<?php echo $row['id_services']; ?>"><i class="fa fa-trash fa-lg"></i> Supprimer</a>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <?php
